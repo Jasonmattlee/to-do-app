5d4a175e44dc1ec3eb032ea773fc9e4704fcc6a8
Jasonmattlee <59672536+Jasonmattlee@users.noreply.github.com>
2020-05-11 16:24:32 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+function onReady() {
+  let toDos = [];
+  let id = 0;
+  const addToDoForm = document.getElementById('addToDoForm');
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id
+    });
+
+    id++;
+
+    newToDoText.value = '';
+
+    renderTheUI();
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      const deleteBTN = document.createElement('button');
+      deleteBTN.textContent = "delete";
+
+      deleteBTN.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        })
+        renderTheUI();
+      });
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBTN);
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+  });
+
+  renderTheUI();
+}
+
+window.onload = function() {
+  onReady();
+};
